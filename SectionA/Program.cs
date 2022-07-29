49ac2c2743ad3daefa90b3b6db2e7b54901b1435
VIONA ERIKA <211285T@mymail.nyp.edu.sg>
2022-07-29 23:38:45 +0800

    updated code to read txt file and return list of employees instead of list of strings

--- SectionA/Program.cs
+++ SectionA/Program.cs
@@ -1,9 +1,10 @@
 ﻿// Viona Erika
 // 211285T
 using System;
-using System.Collections.Generic;
 using System.Linq;
+using System.Collections.Generic;
 using models;
+using System.Globalization;
 
 namespace program;
 
@@ -11,24 +12,39 @@ class program
 {
     static void Main(string[] args)
     {
-        Employee employee = new Employee();
-        employee.generateListForCorpAdmin();
-        List<List<string>> employeeList = readHRMasterList();
+        List<Employee> employeeList = readHRMasterList();
     }
 
-    public static List<List<string>> readHRMasterList()
+    public static List<Employee> readHRMasterList()
     {
         string path = "../HRMasterlist.txt";
-        List<List<string>> employeeList = new List<List<string>>();
+        List<Employee> employeeList = new List<Employee>();
         using Stream fileStream = new FileStream(path, FileMode.Open, FileAccess.Read);
+        // Type employeeType = typeof(Employee);
         using (StreamReader file = new StreamReader(fileStream))
         {
             string line;
-
             while ((line = file.ReadLine()!) != null)
             {
-                List<string> employeeListLine = line.Split("|").ToList();
-                employeeList.Add(employeeListLine);
+                string[] employeeListLine = line.Split("|");
+                employeeList.Add(
+                    new Employee
+                    {
+                        Nric = employeeListLine[0],
+                        FullName = employeeListLine[1],
+                        Salutation = employeeListLine[2],
+                        StartDate = DateTime.ParseExact(
+                            employeeListLine[3],
+                            "dd/MM/yyyy",
+                            CultureInfo.InvariantCulture
+                        ),
+                        Designation = employeeListLine[4],
+                        Department = employeeListLine[5],
+                        MobileNo = employeeListLine[6],
+                        HireType = employeeListLine[7],
+                        Salary = Double.Parse(employeeListLine[8])
+                    }
+                );
             }
             file.Close();
         }
