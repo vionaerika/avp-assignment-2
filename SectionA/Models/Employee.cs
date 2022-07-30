e59fca9bd26ce0f3b97c074a78a106168d843c40
VIONA ERIKA <211285T@mymail.nyp.edu.sg>
2022-07-30 19:03:52 +0800

    reformated the scetion A model

--- SectionA/Models/Employee.cs
+++ SectionA/Models/Employee.cs
@@ -3,51 +3,43 @@
 using System;
 using System.Collections.Generic;
 
-namespace SectionA.Models
+namespace SectionA.Models;
+
+public class Employee
 {
-    public class Employee
+    public string? Nric { get; set; }
+    public string? FullName { get; set; }
+    public string? Salutation { get; set; }
+    public DateTime StartDate { get; set; }
+    public string? Designation { get; set; }
+    public string? Department { get; set; }
+    public string? MobileNo { get; set; }
+    public string? HireType { get; set; }
+    public double? Salary { get; set; }
+    public double? MonthlyPayout { get; set; } = 0.0;
+
+    public string generateStringForCorpAdmin()
     {
-        public string? Nric { get; set; }
-        public string? FullName { get; set; }
-        public string? Salutation { get; set; }
-        public DateTime StartDate { get; set; }
-        public string? Designation { get; set; }
-        public string? Department { get; set; }
-        public string? MobileNo { get; set; }
-        public string? HireType { get; set; }
-        public double? Salary { get; set; }
-        public double? MonthlyPayout { get; set; } = 0.0;
+        string[] requiredData = { FullName!, Designation!, Department! };
+        return (String.Join(",", requiredData));
+    }
 
-        public string generateStringForCorpAdmin()
-        {
-            string[] requiredData = { FullName!, Designation!, Department! };
-            return (String.Join(",", requiredData));
-        }
+    public string generateStringForProcurement()
+    {
+        string[] requiredData = { Salutation!, FullName!, MobileNo!, Designation!, Department! };
+        return (String.Join(",", requiredData));
+    }
 
-        public string generateStringForProcurement()
+    public string generateStringForITDepartment()
+    {
+        string[] requiredData =
         {
-            string[] requiredData =
-            {
-                Salutation!,
-                FullName!,
-                MobileNo!,
-                Designation!,
-                Department!
-            };
-            return (String.Join(",", requiredData));
-        }
-
-        public string generateStringForITDepartment()
-        {
-            string[] requiredData =
-            {
-                Nric!,
-                FullName!,
-                StartDate.ToString("dd/MM/yyyy"),
-                Department!,
-                MobileNo!
-            };
-            return (String.Join(",", requiredData));
-        }
+            Nric!,
+            FullName!,
+            StartDate.ToString("dd/MM/yyyy"),
+            Department!,
+            MobileNo!
+        };
+        return (String.Join(",", requiredData));
     }
 }
